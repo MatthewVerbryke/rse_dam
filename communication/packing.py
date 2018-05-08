c76118e72a2553f7bb635a88d76a03fd7f9d1acf
Matthew Verbryke <verbrymr@mail.uc.edu>
2018-05-08 15:34:15 -0400

    Add 'PoseArray' and 'RobotTrajectory' packing functions.

--- communication/packing.py
+++ communication/packing.py
@@ -7,6 +7,8 @@
     -std_msgs/Header
     -geometry_msgs/Pose
     -geometry_msgs/PoseStamped
+    -geometry_msgs/PoseArray
+    -moveit_msgs/RobotTrajectory
 
   Copyright 2018 University of Cincinnati
   All rights reserved. See LICENSE file at:
@@ -18,6 +20,7 @@
 
 
 from geometry_msgs.msg import Pose, PoseArray, PoseStamped
+from moveit_msgs.msg import RobotTrajectory
 from std_msgs.msg import Header
 
 
@@ -114,7 +117,7 @@ def pack_posestamped(posestamped):
     Package 'geometry_msgs/PoseStamped' message.
     """
     
-    # Get header and pose messages 
+    # Get header and pose messages
     header_msg = pack_header(posestamped.header)
     pose_msg = pack_pose(posestamped.pose)
     
@@ -129,7 +132,7 @@ def unpack_posestamped(dictmsg):
     Unpackage 'geometry_msgs/PoseStamped' message.
     """
     
-    # Get info from message (reusing previous functions)
+    # Get info from message
     posestamped_msg = dictmsg["msg"]
     header_msg = {"msg": posestamped_msg["header"]}
     header = unpack_header(header_msg)
@@ -146,39 +149,113 @@ def unpack_posestamped(dictmsg):
 
 def pack_posearray(posearray):
     """
+    Package 'geometry_msgs/PoseArray' message.
     
+    TODO: TEST
     """
-    pass
+    
+    # Determine if using pose or posestamped
+    pose_type = type(posearray.poses[0])
+    
+    # Get header messages
+    header_msg = pack_header(posearray.header)
+    
+    # Setup loop
+    pose_num = len(posearray.poses)
+    pose_list_msg = [None]*pose_num
+
+    # Get and store a message for each pose
+    for i in range(0,pose_num):
+        if (posetype=="Pose"):
+            pose_msg_i = pack_pose(posearray.poses[i])
+        elif (posetype=="Pose"):
+            pose_msg_i = pack_posestamed(posearray.poses[i])
+        pose_list_msg[i] = pose_msg_i
+        
+    # Package into a dict
+    posearray_msg = {"header": header_msg,
+                     "poses": pose_list_msg}
+                     
+    return posearray_msg
 
 def unpack_posearray(dictmsg):
     """
+    Unpackage 'geometry_msgs/PoseStamped' message.
     
+    TODO: FINISH?
     """
-    pass
     
+    # Get info from message
+    posearray_msg = dictmsg["msg"]
+    header_msg = {"msg": posearray_msg["header"]}
+    header = unpack_header(header_msg)
+    
+    pass
+
 
 def pack_diagnosticarray(datalist):
     """
-    
+    TODO
     """
     pass
     
 def unpack_diagnosticarray(dictmsg):
     """
-    
+    TODO
     """
     pass
 
 
-def pack_robottrajectory(datalist):
+def pack_robottrajectory(robottrajectory):
+    """
+    Package 'geometry_msgs/PoseStamped' message.
+    
+    TODO: TEST
     """
     
-    """
-    pass
+    # Get header message
+    jt_header_msg = pack_header(robottrajectory.joint_trajectory)
     
+    # Get joint name message
+    joint_name_msg = {"joint_names": robottrajectory.joint_trajectory.joint_names}
+    
+    # Setup loop
+    jtpoints_num = len(robottrajectory.joint_trajectory.points)
+    jtpoints_list = [None]*jtpoints_num
+    
+    # Get messages for all JointTrajectoryPoints
+    for i in range(0,jtpoint_num):
+        
+        # Get info from current point
+        current_point = robottrajectory.joint_trajectory.points[i]
+        positions = current_point.positions
+        velocities = current_point.velocities
+        acceleration = current_point.acceleration
+        effort = current_point.effort
+        time_secs = current_point.time_from_start.secs
+        time_nsecs = current_point.time_from_start.nsecs
+        
+        # Package into dict
+        jtpoint_msg = {"positions": positions,
+                       "velocities": velocities,
+                       "accelerations": accelerations,
+                       "effort": effort,
+                       "time_from_start", {"secs", time_secs, "nsecs", time_nsecs}}
+        
+        # Put into list
+        jtpoints_list[i] = jtpoint_msg
+        
+    # Assemble the message
+    rt_msg = {"joint_trajectory": {"header": jt_header_msg,
+                                   "joint_names": joint_name_msg,
+                                   "points": jtpoints_list}
+              "multi_dof_joint_trajectory": {}} # <-- okay being empty?
+              
+    return rt_msg
+
 def unpack_robottrajectory(dictmsg):
     """
-    
+    TODO
     """
     pass
 
