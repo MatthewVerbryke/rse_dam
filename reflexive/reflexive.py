1b4bc735601e275d6d82f07f8957398a33b47541
Matthew Verbryke <verbrymr@mail.uc.edu>
2020-08-12 15:27:53 -0400

    Move ArbotiX driver-related code out of the RSE project.

--- reflexive/reflexive.py
+++ reflexive/reflexive.py
@@ -22,20 +22,16 @@ import thread
 import time
 
 import rospy
-from arbotix_python.arbotix import ArbotiX
 from diagnostic_msgs.msg import DiagnosticArray
-from servos import DynamixelServo
-from interfaces import DynamixelInterface
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory
 
 from csa_msgs.msg import CSADirective, CSAResponse
 
 
-class ArbotiXReflexiveModule(object):
+class ReflexiveModule(object):
     """
-    RSE reflexive layer for a dual arm robot using an Arbotix type micro-
-    controller.
+    RSE reflexive layer for a dual arm robot.
     
     TODO: Test pretty much everything.
     """
@@ -52,11 +48,9 @@ class ArbotiXReflexiveModule(object):
         #TODO
         
         # Retrieve parameters 
-        port = rospy.get_param("~port", "/dev/arbotix")
-        baud = int(rospy.get_param("~baud", "115200"))
         self.rate = rospy.get_param("~rate", 100.0)
-        self.sim = rospy.get_param("~sim", False)
-        self.side = rospy.get_param("~side", "left")
+        self.side = rospy.get_param("~side", "")
+        self.robot = rospy.get_param("~robot", "")
         
         # Setup variables
         self.directives = []
@@ -74,50 +68,12 @@ class ArbotiXReflexiveModule(object):
         # Get a lock
         self.lock = thread.allocate_lock()
         
-        # Setup Arbotix hardware driver if not in simulation
-        if not self.sim:
-            self.driver = ArbotiX.__init__(self, port, baud)
-            rospy.sleep(1.0)
-            rospy.loginfo("Started ArbotiX connection on port " + port + ".")
-            self.setup_hw_interface()
-        else:
-            # TODO
-            rospy.loginfo("ArbotiX being simulated.")
-        
         # Initialize communications
         self.init_comms()
         rospy.loginfo("Communcation interfaces setup")
         
         # Start state machine
         self.run_state_machine()
-        
-    def setup_hw_interface(self):
-        """
-        Setup the required interfaces for communication with the hardware
-        """
-        
-        # Setup joints
-        self.driver.joints = dict{}
-        for name in rospy.get_param("~joints", dict()).keys()
-            self.driver.joints[name] = DynamixelServo(name)
-        
-        # Setup controllers
-        self.driver.controllers = [DynamixelInterface(self.driver, "servos"), ]
-        controllers = rospy.get_param("~controllers", dict())
-        for name, params in controllers.items():
-            try:
-                controller = controller_types[params["type"]](self.driver, name)
-                self.driver.controllers.append(controller)
-                pause = pause or controller.pause
-            except Exception as e:
-                if type(e) == KeyError:
-                    rospy.logerr("Unrecognized controller: " + params["type"])
-                else:  
-                    rospy.logerr(str(type(e)) + str(e))
-        
-        # Start controllers
-        for controller in self.driver.controllers:
-            controller.startup()
 
     def run_state_machine(self):
         """
@@ -151,66 +107,22 @@ class ArbotiXReflexiveModule(object):
         """
         
         # Message topic names
-        joint_state_name = "{}_joint_states".format(self.side)
-        diagnostic_name = "{}_diagnostics".format(self.side)
-        response_name = "{}_arm/RL_response/".format(self.side)
-        directive_name = "{}_arm/HL_directive".format(self.side)
+        response_name = "{}/{}_arm/RL_response".format(self.robot,
+                                                       self.side)
+        directive_name = "{}/{}_arm/HL_directive".format(self.robot, 
+                                                         self.side)
         
         # Setup ROS Publishers
-        self.joint_state_pub = rospy.Publisher(joint_state_name, JointState,
-                                               queue_size=5)
-        self.diagnostic_pub = rospy.Publisher(diagnostic_name, DiagnosticArray,
-                                              queue_size=5)
         self.response_pub = rospy.Publisher(response_name, CSAResponse,
                                             queue_size=1)
         
         # Setup ROS Subscribers
         self.directive_sub = rospy.Subscriber(directive_name, CSADirective,
                                               self.directive_cb)
-    
-    def build_diagnostic_msg(self):
-        """
-        Build a DiagnosticArray message from current data.
-        """
-        
-        # Create message
-        msg = DiagnosticArray()
-        
-        # Fill out message
-        msg.header.stamp = rospy.Time.now()
-        for joint in self.driver.joints:
-            d = joint.get_diagnostics()
-            if d:
-                msg.status.append(d)
-        
-        return msg
-        
-    def build_joint_state_msg(self):
-        """
-        Build a JointState message from the currently up-to-date joint 
-        states.
-        """
-        
-        # Create message
-        msg = JointState()
-        
-        # Fill out message
-        msg.header.stamp() = rospy.Time.now()
-        msg.name = []
-        msg.position = []
-        msg.velocity = []
-        msg.effort = [] # <-- Using this to pub the joint loads
-        for joint in self.driver.joints:
-            msg.name.append(joint.name)
-            msg.position.append(joint.jst_position)
-            msg.velocity.append(joint.velocity)
-            msg.effort.append(joint.load)
-            
-        return msg
         
     def build_response(self):
         """
-        Buiild a CSA response message
+        Build a CSA response message
         """
         
         # Create the response message
