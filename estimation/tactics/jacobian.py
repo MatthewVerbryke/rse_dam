f59a921a734566f9756abcdb9ba8e0eb087a586d
Matthew Verbryke <verbrymr@mail.uc.edu>
2020-10-09 11:13:20 -0400

    Pull down some newer code from devel branch.

---
+++ estimation/tactics/jacobian.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+"""
+  A solver which can solve for the both individual Jacobians and the 
+  relative Jacobian of a dual-arm robot.
+  
+  Copyright 2020 University of Cincinnati
+  All rights reserved. See LICENSE file at:
+  https://github.com/MatthewVerbryke/rse_dam
+  Additional copyright may be held by others, as reflected in the commit
+  history.
+"""
+
+
+import numpy as np
+import rospy
+import tf
+from tf import transformations as trfms
+
+import solutions
+
+
+class DualArmJacobianSolver(object):
+    """
+    The dual-arm Jacobian solver object.
+    """
+    
+    def __init__(self, robot, arms, master):
+        
+        # Parameters
+        self.robot = robot
+        self.left_names = arms["left_arm"]["name"]
+        self.left_index = arms["left_arm"]["index"]
+        self.right_names = arms["right_arm"]["name"]
+        self.right_index = arms["right_arm"]["index"]
+        self.num_joints = len(self.left_names)
+        self.left_base = "/" + arms["left_arm"]["base"]
+        self.left_eef = "/" + arms["left_arm"]["eef"]
+        self.right_base = "/" + arms["right_arm"]["base"]
+        self.right_eef = "/" + arms["right_arm"]["eef"]
+        
+        # Variables
+        self.master = master
+        
+        # Create a TF listener
+        self.listener = tf.TransformListener()
+        
+    def get_relative_jacobian(self, J_A, J_B, R_21, R_24, p_23):
+    def get_relative_jacobian(J_A, J_B, T_21, T_24, p_23):
+        """
+        Get the Relative jacobian matrix of the dual-arm setup. Which 
+        arm is currently the "master" arm is not handled inside this
+        function.
+        
+        Parameters
+        ----------
+        J_A : Arm A Jacobian Matrix
+        J_B : Arm B Jacobian Matrix
+        T_21 : Arm A EEF to Arm A base transformation matrix (Arm A EFF
+               frame)
+        T_24 : Arm A EEF to Arm B base transformation matrix (Arm A EFF
+               frame)
+        p_23 : Arm A EFF to Arm B EFF pose difference (Arm A EFF frame)
+        
+        Returns
+        -------
+        J_R: Relative Jacobian Matrix
+        """
+        
+        # Build required matricies for equation
+        p_23_skew_sym = np.array([[0., -p_23[2], p_23[1]],
+                                 [p_23[0], 0., -p_23[0]],
+                                 [-p_23[1], p_23[0], 0.]])
+        
+        psi_23 = np.identity(6)
+        psi_23[0:3,3:6] = p_23_skew_sym
+        
+        omega_21 = np.zeros((6,6))
+        omega_21[0:3,0:3] = T_21[0:3,0:3]
+        omega_21[3:6,3:6] = T_21[0:3,0:3]
+        
+        omega_24 = np.zeros((6,6))
+        omega_24[0:3,0:3] = T_24[0:3,0:3]
+        omega_24[3:6,3:6] = T_24[0:3,0:3]
+        
+        # Calculate the relative Jacobian
+        psi_23_omega_21 = np.multiply(psi_23, omega_21)
+        J_R_left = -np.multiply(psi_23_omega_21, J_A)
+        J_R_right = np.multiply(omega_24, J_B)
+        J_R = np.hstack((J_R_left, J_R_right))
+        
+        return J_R
+        
+    def lookup_frame_transform(self, origin, target):       
+        """
+        Get the transform (translation and quaternion rotation) between 
+        two frames using tf.
+        """   
+        
+        try:
+            self.listener.waitForTransform(target, origin, rospy.Time(0), rospy.Duration(4.0))
+            trans, rot = self.listener.lookupTransform(target, origin, rospy.Time(0))
+            print trans, rot
+            return [trans, rot]
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logerr("Failed to recieve the transform for {} to {}".format(origin, target))
+            return [None, None]
+        
+    def update(self, cur_state):
+        """
+        Update the function when called from the main script.
+        """
+        
+        # Get the trimmed down joint positions
+        q_left = [0.0]*self.num_joints
+        q_right = [0.0]*self.num_joints
+        for i in range(0,self.num_joints):
+            q_left[i] = cur_state.left_joint_state.position[self.left_index[i]]
+            q_right[i] = cur_state.right_joint_state.position[self.right_index[i]]
+        
+        # Get the individual arm Jacobians (TODO: improve this)
+        J_left = solutions.solve_boxbot_6dof(q_left)
+        J_right = solutions.solve_boxbot_6dof(q_right)
+            
+        # Determine relavant frame transformations
+        if self.master == "left":
+            J_A = J_left
+            J_B = J_right
+            trans_21 = self.lookup_frame_transform(self.left_eef, self.left_base)
+            trans_24 = self.lookup_frame_transform(self.left_eef, self.right_base)
+            trans_23 = self.lookup_frame_transform(self.left_eef, self.right_eef)
+        elif self.master == "right":
+            J_A = J_right
+            J_B = J_left
+            trans_21 = self.lookup_frame_transform(self.right_eef, self.right_base)
+            trans_24 = self.lookup_frame_transform(self.right_eef, self.left_base)
+            trans_23 = self.lookup_frame_transform(self.right_eef, self.left_eef)
+        else:
+            return [J_left, J_right, None], "ok"
+            
+        # Determine the relative Jacobian
+        R_21 = trfms.quaternion_matrix(trans_21[1])
+        R_24 = trfms.quaternion_matrix(trans_24[1])
+        p_23 = trans_23[0]
+        J_R = self.get_relative_jacobian(J_left, J_right, R_21, R_24, p_23)
+
+        return [J_left, J_right, J_R], "ok"
--- multilaunch.py
+++ multilaunch.py
@@ -2,15 +2,14 @@
 
 """
   A script to run various other specified scripts and open them together.
-  WIP
-  
-  Modified from:
-  https://github.com/cmcghan/rss_git_lite/blob/master/multiopen_old_combined.py
-  
-  Copyright 2018 University of Cincinnati
+
+  Copyright 2020 University of Cincinnati
   All rights reserved. See LICENSE file at:
   https://github.com/MatthewVerbryke/rse_dam
-  Additional copyright may be held by others, as reflected in the commit history.
+  Additional copyright may be held by others, as reflected in the commit
+  history.
+  
+  TODO: Test
 """
 
 
@@ -24,242 +23,112 @@ sys.path.append(os.getcwd() + "/..")
 from rss_git_lite.common import fileFunctions as fF
 
 
-def printInstructionsToScreen(runScriptType):
-    """ """
+def print_instructions_to_screen():
+    """
+    TODO
+    """
     pass
     
+def prepare_rosbridge_server(top_dir):
+    """
+    Prepare to launch a rosbridge server
+    """
+        
+    tab_title = []
+    command = []
+    
+    # Rosbridge server
+    tab_title.extend(["rosbridge_server"])
+    command.extend([''' source %(top_dir)s/devel/setup.bash
+                        cd utilities
+                        roslaunch rosbridge_server_9090.launch
+    ''' % locals()])
+    
+    return tab_title, command
+
+def prepare_state_estimator(top_dir, param_files, param_dir):
+    """
+    Prepare commands to launch all state estimation module components
+    """
+    
+    tab_title = []
+    command = []
+    
+    # State Estimator
+    tab_title.extend(["state_estimation"])
+    command.extend([''' source %(top_dir)s/devel/setup.bash
+                        cd estimation
+                        python state_estimation.py %(param_files)s %(param_dir)s
+    ''' % locals()])
+    
+    return tab_title, command
+
+
 if __name__ == "__main__":
     
-    # Get important directories
-    topdir = "../.." # where toplevel stuff needs to get to
-    cddir = "./src/rse_dam" # where we start
+    # Get import directory paths
+    top_dir = "../.." # where toplevel stuff needs to get to
+    rse_dir = "./src/rse_dam" # where we start
     
-    print("Starting multilaunch.py...")
+    # Storage for launch information
+    groups = []
+    
+    # Input parameters (TODO: improve)
+    param_files = "arm_6dof,connections,default"
+    param_dir = "/home/matthew/catkin_ws/src/boxbot_ros/boxbot_bringup/config"
+    
+    # launch parameters (TODO: improve)
+    launch_rosbridge = False
+    launch_SEM = True
+    launch_DL = False
+    launch_HL = False
+    launch_RL = False
     
     try:
-        print("sys.argv = " + str(sys.argv))
-
-        # Get file argument values
-        argvdefaults = ["Friday2",
-                        "gazebo",
-                        "empty_world.launch",
-                        "moveit.py",
-                        "deliberative.py"] # default connection valid because starting rosbridge from this file :)
-        [computer_name,
-         simulated,
-         gazebo_world,
-         HL_call,
-         DL_call] = fF.readArgvpieces(sys.argv,argvdefaults)
-         
-        # General parameters (no alternatives ATM)
-        robot_type = "boxbot"
-        joint_velocity_limits = "[0.785, 1.571, 1.571, 1.571, 1.571]"
         
-        # Gazebo parameters
-        sim_type = "gazebo"
-        sim_ref_frame = "world"
-        left_eef_frame = "left_wrist_2_link"
-        right_eef_frame = "right_wrist_2_link"
+        # Get the rosbridge server launch information
+        if launch_rosbridge:
+            tab_title, command = prepare_rosbridge_server(top_dir)
+            groups.append((tab_title, command))
         
-        # Habitual module parameters (no alternatives ATM)
-        arm_group = "widowx_arm"
-        gripper_group = "widowx_gripper"
-        HL_ref_frame = "origin_point"
-        launch_rviz = True
-        joint_names = "['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5']"
-        
-        # Determine parameters dependent on computer
-        if (computer_name=="Friday2"):
-            print ("This is the main simulation computer.")
-            left_arm = True
-            launch_gazebo = True
-            launch_DL = True
-            launch_state_pub = True
-            launch_arm_command = False
-            connection = "ws://192.168.0.50:9090/"
-        elif (computer_name=="cameronhurd"):
-            print ("This is the secondary computer.")
-            left_arm = False
-            launch_gazebo = False
-            launch_DL = False
-            launch_state_pub = False
-            launch_arm_command = True
-            connection = "ws://192.168.0.51:9090/"
-        
-        # Determine if we are running a Gazebo simulation, running on actual
-        # hardware, or only using fake drivers in RViz. If Gazebo is running,
-        # sim is false due to it interfacing similar to actual hardware.
-        if (simulated=="true"): #<--RViz only
-            sim = True
-            DL_ref_frame = HL_ref_frame
-            launch_gazebo = False
-            launch_state_pub = False
-            gazebo_running = False
-            launch_arm_command = False
-        elif (gazebo_world=="real"): #<--actual hardware
-            sim = False
-            DL_ref_frame = HL_ref_frame
-            launch_gazebo = False
-            gazebo_running = False
-            launch_state_pub = False
-            launch_arm_command = False
-        else:  #<-- Gazebo simulation
-            sim = False
-            gazebo_running = True
-            DL_ref_frame = sim_ref_frame
-                  
-        # run catkin_make FIRST in the original terminal window and wait
-        # 'til it's complete before doing any else! (otherwise you'll get
-        # weird ROS ID error messages from the roscore and rosbridge_server
-        # if the messages were changed/updated but not recompiled)
-        #command = ['bash', '-c', '''
-                        #cd %(topdir)s
-                        #source devel/setup.bash && catkin_make
-        #''' % locals()]
+        # Get the state estimator module(s) launch information
+        if launch_SEM:
+            tab_title, command = prepare_state_estimator(top_dir, param_files, param_dir)
+            groups.append((tab_title, command))
             
-        #subprocess.call(command) # each call originates from original directory
-                                 # so "cd" here is "ignored" further down :)
+        # Get the reflexive layer module(s) launch information
+        if launch_RL:
+            pass #TODO
         
-        # TODO: print instructions to screen
+        # Get the habitual layer module(s) launch information
+        if launch_HL:
+            pass #TODO
         
-        #== TERMINAL 1 =================================================
-        
-        tabtitle = []
-        command = []
-        
-        # If we are using Gazebo, prepare needed script(s)
-        if launch_gazebo:
-            
-            # Gazebo
-            tabtitle.extend(["gazebo"])
-            command.extend([''' source %(topdir)s/devel/setup.bash
-                                roslaunch boxbot_gazebo %(gazebo_world)s
-            ''' % locals()])
-        
-        # Prepare the rosbridge server
-        tabtitle.extend(["rosbridge_server (status)"])
-        command.extend([''' source %(topdir)s/devel/setup.bash
-                            roslaunch rosbridge_server_9090.launch
-        ''' % locals()])
-        
-        terminal = ['gnome-terminal']
-        for i in range(len(command)):
-            terminal.extend(['--tab', '-e', '''
-                bash -c '
-                    %s
-                    read
-                '
-            ''' % (command[i],), '-t', '%s' % (tabtitle[i],)])
-                
-        # Launch rosbridge server and (maybe) Gazebo
-        subprocess.call(terminal)
-        sleep(8)
-        
-        #== TERMINAL 2 =================================================
-        
-        tabtitle = []
-        command = []
-        
-        # Setup the Move group and RL scripts (assumes you are running an arbotix controller)
-        tabtitle.extend(["movegroup/RL"])
-        command.extend([''' source %(topdir)s/devel/setup.bash
-                            roslaunch widowx_arm_bringup boxbot_arms_moveit.launch sim:=%(sim)s gazebo:=%(gazebo_running)s left_arm:=%(left_arm)s
-
-        ''' % locals()])
-        
-        # Arm state publisher
-        if launch_state_pub:
-            tabtitle.extend(["arm-state"])
-            command.extend([''' cd communication
-                                python arm_state.py %(connection)s %(left_arm)s %(robot_type)s
-            ''' % locals()])
-        # Clock publisher    
-        if launch_gazebo:
-            tabtitle.extend(["clock publisher"])
-            command.extend([''' cd communication
-                                python clock.py %(connection)s
-            ''' % locals()])
-            
-        # Arm command
-        if launch_arm_command:
-            tabtitle.extend(["arm-command"])
-            command.extend([''' cd communication
-                                python arm_commands.py %(connection)s %(left_arm)s %(robot_type)s
-            ''' % locals()])
-        
-            # Wait for the state publisher on the left computer start first
-            sleep(2)
-        
-        # Ready the new terminal
-        terminal2 = ['gnome-terminal']
-        for i in range(len(command)):
-            terminal2.extend(['--tab', '-e', '''
-                bash -c '
-                    %s
-                    read
-                '
-            ''' % (command[i],), '-t', '%s' % (tabtitle[i],)])
-            
-        # Launch all the previous commands in a multitab terminal
-        subprocess.call(terminal2)
-        sleep(9)
-        
-        #== TERMINAL 3 =================================================
-
-        tabtitle = []
-        command = []
-        
-        # Setup the Habitual module/ MoveIt interface
-        tabtitle.extend(["habitual"])
-        command.extend([''' cd habitual
-                            python %(HL_call)s %(arm_group)s %(gripper_group)s %(HL_ref_frame)s %(left_arm)s %(connection)s
-        ''' % locals()])
-        
-        # Ready the new terminal
-        terminal3 = ['gnome-terminal']
-        for i in range(len(command)):
-            terminal3.extend(['--tab', '-e', '''
-                bash -c '
-                    %s
-                    read
-                '
-            ''' % (command[i],), '-t', '%s' % (tabtitle[i],)])
-        
-        subprocess.call(terminal3)
-        sleep(4)
-        
-        #== TERMINAL 4 =================================================
-        
-        tabtitle = []
-        command = []
-        
-        # Setup the deliberative module
+        # Get the deliberative layer module(s) launch information
         if launch_DL:
-            tabtitle.extend(["deliberative"])
-            command.extend([''' cd deliberative
-                                python %(DL_call)s %(DL_ref_frame)s %(left_eef_frame)s %(right_eef_frame)s %(connection)s
-            ''' % locals()])
+            pass #TODO
             
-            # Ready the new terminal
-            terminal4 = ['gnome-terminal']
-            for i in range(len(command)):
-                terminal4.extend(['--tab', '-e', '''
+        # Launch all desired components
+        for group in groups:
+            terminal = ["gnome-terminal"]
+            for i in range(len(group[1])):
+                terminal.extend(['--tab', '-e', '''
                     bash -c '
                         %s
                         read
                     '
-                ''' % (command[i],), '-t', '%s' % (tabtitle[i],)])
+                ''' % (group[1][i],), '-t', '%s' % (group[0][i],)])
+            #print terminal
+            subprocess.call(terminal)
+            sleep(3)
             
-            subprocess.call(terminal4)
-            sleep(4)
-        
-        print("All init-scripts have now been called.")
+        print("All desired components have now been called.")
     
+    # Handle exceptions
     except KeyboardInterrupt:
         try:
             print("Keyboard interrupt command recieved, stopping script.")
         except NameError:
-            print("NameError")
-
+            print("Name Error")
         sleep(3)
 
